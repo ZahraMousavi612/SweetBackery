0d4959d88f44ca8d55ebdb9a97529a549941084a
ZahraMousavi612 <>
2023-06-13 13:03:02 +0000

    change the project name to sweet_backery

--- sweet_backery/asgi.py
+++ sweet_backery/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for home_ease project.
+ASGI config for sweet_backery project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_ease.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sweet_backery.settings')
 
 application = get_asgi_application()
